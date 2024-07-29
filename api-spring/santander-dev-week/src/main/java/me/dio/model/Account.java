db2769a9299385ecdee1388048b1169427cbf19e
murillohc <167935010+murillohc@users.noreply.github.com>
2024-07-29 12:31:24 -0300

    Add files via upload

---
+++ api-spring/santander-dev-week/src/main/java/me/dio/model/Account.java
@@ -0,0 +1,71 @@
+package me.dio.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String number;
+
+    @Column(nullable = false)
+    private String agency;
+
+    @Column(nullable = false, precision = 13, scale = 2)
+    private BigDecimal balance;
+
+    @Column(name = "additional_limit", nullable = false, precision = 13, scale = 2)
+    private BigDecimal limit;
+
+    public Account() {
+    }
+
+    public Account(String number, String agency, BigDecimal balance, BigDecimal limit) {
+        this.number = number;
+        this.agency = agency;
+        this.balance = balance;
+        this.limit = limit;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
---
+++ api-spring/santander-dev-week/src/main/java/me/dio/model/User.java
@@ -0,0 +1,93 @@
+package me.dio.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<News> news;
+
+    public User(){
+    }
+
+    public User(String name, Account account, List<Feature> features, Card card, List<News> news) {
+        this.name = name;
+        this.account = account;
+        this.features = features;
+        this.card = card;
+        this.news = news;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
